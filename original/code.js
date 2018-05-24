4c7b25a163b2a25ed1645dac7f94126369f01fd5
Jenny Lea <jennylea@Jennys-MBP.lan>
2018-05-24 10:02:19 +0100

    COmmented out uk hexmap bit of code as it doesnt do anything.

--- original/code.js
+++ original/code.js
@@ -1,44 +1,44 @@
-var HexMap = (function () {
-  var ANGLE = (60 * (Math.PI / 180.0));
-
-  function closedPath(point, points) {
-    var tmp = [];
-
-    tmp.push('M' + point[0] + ' ' + point[1]);
-
-    for (var i = 0; i < points.length; i++) {
-      var point = points[i];
-
-      tmp.push('L' + point[0] + ' ' + point[1]);
-    }
-
-    tmp.push('z');
-
-    return tmp.join('');
-  }
-
-  return function (canvas, ox, oy, size, fillfn) {
-    var ssin = size * Math.sin(ANGLE);
-
-    var scos = size * Math.cos(ANGLE);
-
-    for (var i = 0; i < 650; i++) {
-      var constituency = UK_CONSTITUENCIES[i];
-
-      var x = ox + (constituency[0] * (size + scos)), y = oy - (constituency[1] * ssin);
-
-      var x1 = x + size, y1 = y + ssin, y2 = y1 + ssin;
-
-      var hexagon = closedPath([x, y], [[x1, y], [x1 + scos, y1], [x1, y2], [x, y2], [x - scos, y1]]);
-
-      canvas.path(hexagon).attr({stroke: '#AAAAAA', fill: fillfn(constituency[2]), title: constituency[2]});
-    }
-  }
-})();
+// var HexMap = (function () {
+//   var ANGLE = (60 * (Math.PI / 180.0));
+//
+//   function closedPath(point, points) {
+//     var tmp = [];
+//
+//     tmp.push('M' + point[0] + ' ' + point[1]);
+//
+//     for (var i = 0; i < points.length; i++) {
+//       var point = points[i];
+//
+//       tmp.push('L' + point[0] + ' ' + point[1]);
+//     }
+//
+//     tmp.push('z');
+//
+//     return tmp.join('');
+//   }
+//
+//   return function (canvas, ox, oy, size, fillfn) {
+//     var ssin = size * Math.sin(ANGLE);
+//
+//     var scos = size * Math.cos(ANGLE);
+//
+//     for (var i = 0; i < 650; i++) {
+//       var constituency = UK_CONSTITUENCIES[i];
+//
+//       var x = ox + (constituency[0] * (size + scos)), y = oy - (constituency[1] * ssin);
+//
+//       var x1 = x + size, y1 = y + ssin, y2 = y1 + ssin;
+//
+//       var hexagon = closedPath([x, y], [[x1, y], [x1 + scos, y1], [x1, y2], [x, y2], [x - scos, y1]]);
+//
+//       canvas.path(hexagon).attr({stroke: '#AAAAAA', fill: fillfn(constituency[2]), title: constituency[2]}).attr('id', 'jenny');
+//     }
+//   }
+// })();
 
 var loadVisualisation = function(callback) {
 
-  var svg = d3.select('#frame').append('svg').attr('width', 500).attr('height', 430);
+  var svg = d3.select('#frame').append('svg').attr('width', 500).attr('height', 500).attr('transform', 'scale(1.5)');
   var map = UK.HexMap();
   document.getElementById('heading').innerHTML = variables[variable]['title'];
 
@@ -97,14 +97,14 @@ var loadVisualisation = function(callback) {
 
   map.stroke('#333');
 
-  map.origin({x:100, y:395});
+  map.origin({x:150, y:475});
 
   map(svg);
 
   var legendRectWidth = 50;
   var legendRectHeight = 15;
-  var legendX = 325;
-  var legendY = 50;
+  var legendX = 360;
+  var legendY = 150;
 
   var legend = d3.select('svg')
     .append("g")
@@ -151,4 +151,3 @@ var loadVisualisation = function(callback) {
 }
 
 // FOR TESTING LOCALLY
-//window.onload = loadVisualisation(function() { return 0;});
